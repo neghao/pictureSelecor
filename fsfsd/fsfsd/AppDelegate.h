b93f5be8fe62f100bcebbf287f25e9204decaad5
neghao <neghaoimac@neghaodeiMac.local>
2017-11-26 09:28:29 +0800

    first commit

---
+++ fsfsd/fsfsd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fsfsd
+//
+//  Created by simope on 16/6/8.
+//  Copyright © 2016年 NegHao.W. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pictureSelecor/pictureSeletView.m
@@ -0,0 +1,21 @@
+//
+//  pictureSeletView.m
+//  pictureSelecor
+//
+//  Created by simope on 16/6/8.
+//  Copyright © 2016年 NegHao.W. All rights reserved.
+//
+
+#import "pictureSeletView.h"
+
+@implementation pictureSeletView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
